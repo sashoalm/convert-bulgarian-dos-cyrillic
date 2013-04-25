b32583f00580e7ba270f4864ea5444ecdae68cf2
sashoalm <sashoalm@sashoalm-Aspire-5740.(none)>
2013-04-25 21:35:22 +0300

    Show only the file name in the list widget, without the full file path.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include "doscyrillic.h"
 #include <QtGui/QFileDialog>
+#include <QFileInfo>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -17,7 +18,17 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    ui->listWidget->addItems(QFileDialog::getOpenFileNames(this, "", "", "Assembly files (*.asm);;Text files (*.txt);;All files (*.*)"));
+    // Show the open file dialog.
+    const QStringList &fileNames = QFileDialog::getOpenFileNames(this, "", "", "Assembly files (*.asm);;Text files (*.txt);;All files (*.*)");
+
+    // Because full file paths look ugly, we add only the file name without the
+    // path. The full file path can still be seen as a tooltip.
+    foreach (const QString &fileName, fileNames) {
+        QListWidgetItem *item = new QListWidgetItem;
+        item->setText(QFileInfo(fileName).fileName());
+        item->setToolTip(fileName);
+        ui->listWidget->addItem(item);
+    }
 }
 
 void MainWindow::on_pushButton_2_clicked()
@@ -27,12 +38,14 @@ void MainWindow::on_pushButton_2_clicked()
 
 void MainWindow::on_pushButton_3_clicked()
 {
+    // The full file path is contained in the tooltip.
     for (int ii = 0; ii < ui->listWidget->count(); ii++)
-        dosFileToUtf8(ui->listWidget->item(ii)->text());
+        dosFileToUtf8(ui->listWidget->item(ii)->toolTip());
 }
 
 void MainWindow::on_pushButton_4_clicked()
 {
+    // The full file path is contained in the tooltip.
     for (int ii = 0; ii < ui->listWidget->count(); ii++)
-        utf8FileToDos(ui->listWidget->item(ii)->text());
+        utf8FileToDos(ui->listWidget->item(ii)->toolTip());
 }
